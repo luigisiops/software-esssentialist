ec1ce4390b18ec8e964d9a30fa0cefbf383fe389
Lsiopongco <luigisiops@gmail.com>
2023-05-08 17:55:45 -0400

    [New]: palindrome knows palindromes with different letter casings are still true

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,6 +1,7 @@
 import { reverse } from "dns";
 
 export function palindrome(string: string) {
+    string = string.toLowerCase()
     const splitString = string.split("")
     const reversedString = (splitString.reverse()).join("")
     return string == reversedString
