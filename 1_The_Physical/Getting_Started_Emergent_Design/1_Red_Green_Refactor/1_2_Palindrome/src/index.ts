6c78e47a2abe9b0307d3058333f6083ebf4957ea
Lsiopongco <luigisiops@gmail.com>
2023-05-09 19:27:43 -0400

    [Refactor]: adjusted functions to be more concise, chained split, reverse and join in one line

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,9 +1,7 @@
 import { reverse } from "dns";
 
 export function palindrome(string: string) {
-    string = string.split(" ").join("")
-    string = string.toLowerCase()
-    const splitString = string.split("")
-    const reversedString = (splitString.reverse()).join("")
-    return string == reversedString
+    string = string.split(" ").join("").toLowerCase()
+    const reverseString = string.split("").reverse().join("")
+    return string == reverseString
 }
