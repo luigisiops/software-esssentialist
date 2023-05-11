646054c1cdee825e0632b0c5a434a6dc03a3e75c
Lsiopongco <luigisiops@gmail.com>
2023-05-11 03:18:14 -0400

    [Refactor]: separated out function logic into smaller functions

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
@@ -1,43 +1,54 @@
-interface IError {
+type Error = {
     type: string;
     message: string;
 }
 
 export function passwordValidator(password:string){
-    let result: boolean = true;
-    let errors:IError[] = [];
+    let errors:Error[] = [];
 
-    if (password.length < 5 || password.length > 15) {
+    if (checkPasswordLength(password)) {
         errors.push({
             type: 'Invalid Length',
             message: 'Password length must be between 5 and 15 characters'
         })
     }
 
-    if (password == password.toLowerCase()) {
+    if (!checkHasCapitalLetter(password)) {
         errors.push({
             type: 'No Capital Letter',
             message: 'Password needs to have at least 1 capital letter'
         })
     }
 
-    let hasNumber = false
-    const numbers = new Set(['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'])
-    for (let i = 0; i<password.length; i++) {
-        let char = password[i]
-        if (numbers.has(char)){
-            hasNumber = true
-        }
-    }
-    if (!hasNumber) {
+    if (!checkHasDigit(password)) {
         errors.push({
             type: 'Needs at least 1 Digit',
             message: 'Password needs to have at least 1 digit'
         })
     }
-    
+
     return {
-        result: errors.length == 0 ? true : false,
+        result: errors.length == 0,
         errors: errors
     }
+}
+
+function checkPasswordLength(password: string) {
+    return password.length < 5 || password.length > 15
+}
+
+function checkHasCapitalLetter(password: string) {
+    return password != password.toLowerCase()
+}
+
+function checkHasDigit(password: string) {
+    let hasNumber = false
+    const numbers = new Set(['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'])
+    for (let i = 0; i < password.length; i++) {
+        let char = password[i]
+        if (numbers.has(char)) {
+            hasNumber = true
+        }
+    }
+    return hasNumber
 }
