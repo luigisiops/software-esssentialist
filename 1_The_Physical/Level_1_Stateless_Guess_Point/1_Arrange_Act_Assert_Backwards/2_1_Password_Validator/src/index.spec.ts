76a887dd3426e73a75853a277713b77c30bcd6f5
Lsiopongco <luigisiops@gmail.com>
2023-05-11 16:15:06 -0400

    [Refactor]: Organized related tests into separate describe statements

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -13,52 +13,65 @@ Passwords must meet the following criteria:
 import {passwordValidator} from './index';
 
 describe('password validator', () => {
-  it('throws error when password is not between 5 and 15 characters long', () => {
-    const shortPasswordObject = passwordValidator('dog1')
-    const longPasswordObject = passwordValidator('somelongpassword123')    
-    const returnObject = passwordValidator('Password1')
-    expect(shortPasswordObject.result).toBeFalsy()
-    expect(shortPasswordObject.errors[0].type).toEqual('Invalid Length')
-    expect(shortPasswordObject.errors[0].message).toContain('5 and 15')
+  describe('A valid password must be between 5 and 15 characters long', () => {
+    it('knows that Dog1 is too short of a password', () => {
+      const shortPasswordObject = passwordValidator('Dog1')
+      expect(shortPasswordObject.result).toBeFalsy()
+      expect(shortPasswordObject.errors[0].type).toEqual('Invalid Length')
+      expect(shortPasswordObject.errors[0].message).toContain('5 and 15')
+    })
 
-    expect(longPasswordObject.result).toBeFalsy()
-    expect(longPasswordObject.errors[0].type).toEqual('Invalid Length')
-    expect(longPasswordObject.errors[0].message).toContain('5 and 15') 
-    
-    expect(returnObject.result).toBeTruthy()
-    expect(returnObject.errors.length).toEqual(0)
+    it('knows that Somelongpassword123 is a too long for a password', () => {
+      const longPasswordObject = passwordValidator('somelongpassword123') 
+      expect(longPasswordObject.result).toBeFalsy()
+      expect(longPasswordObject.errors[0].type).toEqual('Invalid Length')
+      expect(longPasswordObject.errors[0].message).toContain('5 and 15')    
+    })
+
+    it('knows that Password1 is a valid password length', () => {
+      const returnObject = passwordValidator('Password1')
+      expect(returnObject.result).toBeTruthy()
+      expect(returnObject.errors.length).toEqual(0)
+    })
   })
 
+  describe('A valid password must have at least 1 capital letter', () => {
+    it('knows that password: password does not have 1 capital letter', () => {
+      const failResponse = passwordValidator('password')
+      expect(failResponse.result).toBeFalsy()
+      expect(failResponse.errors[0].type).toEqual('No Capital Letter')
+      expect(failResponse.errors[0].message).toContain('1 capital letter')
+    })
 
-  test('that password has at least 1 capital letter', () => {
-    const failResponse = passwordValidator('password')
-    const successResponse = passwordValidator('ThisWorks12')
-    const allDigitsResponse = passwordValidator('1234567890')
-    expect(failResponse.result).toBeFalsy()
-    expect(failResponse.errors[0].type).toEqual('No Capital Letter')
-    expect(failResponse.errors[0].message).toContain('1 capital letter')
+    it('knows that ThisWorks12 is a valid password', () => {
+      const successResponse = passwordValidator('ThisWorks12')
+      expect(successResponse.result).toBeTruthy()
+      expect(successResponse.errors.length).toEqual(0)
+    })
 
-    expect(successResponse.result).toBeTruthy()
-    expect(successResponse.errors.length).toEqual(0)
+    it('knows that 1234567890 is not a valid password', () => {
+      const allDigitsResponse = passwordValidator('1234567890')
+      expect(allDigitsResponse.result).toBeFalsy()
+      expect(allDigitsResponse.errors[0].type).toEqual('No Capital Letter')
+      expect(allDigitsResponse.errors[0].message).toContain('1 capital letter')
+    })
+  })
 
-    expect(allDigitsResponse.result).toBeFalsy()
-    expect(allDigitsResponse.errors[0].type).toEqual('No Capital Letter')
-    expect(allDigitsResponse.errors[0].message).toContain('1 capital letter')
+  describe('A valid password must have at least 1 digit', () => {
+    it('knows that SecurePass is not a valid password', () => {
+      const failResponse = passwordValidator('SecurePass')
+      expect(failResponse.result).toBeFalsy()
+      expect(failResponse.errors[0].type).toEqual('Needs at least 1 Digit')
+      expect(failResponse.errors[0].message).toContain('1 digit')  
+    })
+
+    it('knows that SecurePass1 is a valid password', ()=> {
+      const successResponse = passwordValidator('SecurePass1')
+      expect(successResponse.result).toBeTruthy()
+      expect(successResponse.errors.length).toEqual(0)
+    })
   })
   
-  test('that password has at least 1 digit', () => {
-    const failResponse = passwordValidator('SecurePass')
-    const successResponse = passwordValidator('SecurePass1')
-
-    expect(failResponse.result).toBeFalsy()
-    expect(failResponse.errors[0].type).toEqual('Needs at least 1 Digit')
-    expect(failResponse.errors[0].message).toContain('1 digit')  
-
-    expect(successResponse.result).toBeTruthy()
-    expect(successResponse.errors.length).toEqual(0)
-
-  })
-
   it('should throw all three error messages given password: dog', () => {
     const responseObject = passwordValidator('dog')
 
