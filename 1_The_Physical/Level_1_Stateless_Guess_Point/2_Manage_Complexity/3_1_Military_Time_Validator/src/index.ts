8a48632d77997855cb17b93b9b45b51ae7a0d7ac
Lsiopongco <luigisiops@gmail.com>
2023-05-15 15:35:33 -0400

    [Refactor]: created separate method for splitting hours and mins, turned conditionals to one line return statements

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -7,22 +7,20 @@ export class TimeValidator{
 
     exec(time:string) {
         const timeStringFormatLength = 13;
-        if (time.length != timeStringFormatLength) {
-            return false
-        }
+        if (time.length != timeStringFormatLength) return false
 
         const [startTime, endTime] = time.split(" - ")
-        if (!startTime.includes(":") || !endTime.includes(":")) {
-            return false
-        }
+        if (!startTime.includes(":") || !endTime.includes(":")) return false
         
-        const [startHours, startMinutes] = startTime.split(":").map((val) => parseInt(val))
-        const [endHours, endMinutes] = endTime.split(":").map((val) => parseInt(val) )
-        if (startHours > 24 || endHours > 24 || startMinutes > 59 || endMinutes > 59) {
-            return false
-        }
-
+        const [startHours, startMinutes] = this.splitHoursAndMinutes(startTime)
+        const [endHours, endMinutes] = this.splitHoursAndMinutes(endTime)
+        if (startHours > 24 || endHours > 24 || startMinutes > 59 || endMinutes > 59) return false
+        
         return true
     } 
+
+    splitHoursAndMinutes(text: string) {
+        return text.split(":").map((val) => parseInt(val))
+    }
 
 }
