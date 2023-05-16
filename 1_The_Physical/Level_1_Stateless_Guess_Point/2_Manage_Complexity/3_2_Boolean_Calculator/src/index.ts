e21ff176203139b4dffb54dfb8a93a6a9c8d1f86
Lsiopongco <luigisiops@gmail.com>
2023-05-16 17:11:01 -0400

    [Refactor]: cleaned up code, separated out logic for recursive calls into if statements

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
@@ -6,11 +6,18 @@ export class BooleanCalculator {
     }
 
     exec(text: string): boolean{
+        let left
+        let right
+        if (text.split(" AND ").length > 1) {
+            [left, right] = text.split(" AND ")
+            return this.exec(left) && this.exec(right)
+        }
+        if (text.split(" OR ").length > 1) {
+            [left, right] = text.split(" OR ")
+            return this.exec(left) || this.exec(right)
+        }
+
         const splitText = text.split(" ")
-        const splitAnd = text.split(" AND ")
-        const splitOR = text.split(" OR ")
-        if (splitAnd.length > 1) return this.exec(splitAnd[0]) && this.exec(splitAnd[1])
-        if (splitOR.length > 1) return this.exec(splitOR[0]) || this.exec(splitOR[1])
         if (splitText[0] === 'NOT') return !(splitText[1] === 'TRUE' ? true : false)
         if (text === 'TRUE') return true
         else return false
