7e79b20ecc32f3f61622d30ab5c1bd87905a66cd
Lsiopongco <luigisiops@gmail.com>
2023-05-26 19:21:48 -0400

    [Refactor]: created Traffic instance before each test

--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TrafficLight/src/index.spec.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TrafficLight/src/index.spec.ts
@@ -1,19 +1,21 @@
 
 import {TrafficLight} from './index'
 describe('Traffic Light States', () => {
+    let trafficLight: TrafficLight
+
+    beforeEach(() => {
+        trafficLight = TrafficLight.create()
+    })
     it('knows when a traffic light is off', () => {
-        const trafficLight = TrafficLight.create()
         expect(trafficLight.getState()).toBe('off')
     })
 
     it('knows to change state from off to green when a cycle starts', ()=> {
-        let trafficLight = TrafficLight.create()
         trafficLight.startCycle()
         expect(trafficLight.getState()).toBe('green')
     })
 
     it('knows that after 30 seconds have passed and the traffic light is on and currently green to switch to yellow', () => {
-        let trafficLight = TrafficLight.create()
         trafficLight.startCycle()
         jest.advanceTimersByTime(30000)
         expect(trafficLight.getState()).toBe('yellow')
