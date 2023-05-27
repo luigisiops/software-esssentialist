a9e30b53a974acd2bffcf682f78d4b847996642c
Lsiopongco <luigisiops@gmail.com>
2023-05-26 20:39:43 -0400

    [Refactor]: start cycle before each, abstracted initial state test to own describe statement

--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TrafficLight/src/index.spec.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TrafficLight/src/index.spec.ts
@@ -3,29 +3,32 @@ import {TrafficLight} from './index'
 describe('Traffic Light States', () => {
     let trafficLight: TrafficLight
 
-    beforeEach(() => {
-        trafficLight = TrafficLight.create()
+
+    beforeEach(() => {        
         jest.useFakeTimers()
-    })
-    it('knows when a traffic light is off', () => {
-        expect(trafficLight.getState()).toBe('off')
+        trafficLight = TrafficLight.create()
+        trafficLight.startCycle()
+    })    
+    describe('Traffic Light Initial State', () => {
+        it('knows when a traffic light is off', () => {
+            trafficLight = TrafficLight.create()
+            expect(trafficLight.getState()).toBe('off')
+        })
     })
 
     it('knows to change state from off to green when a cycle starts', ()=> {
-        trafficLight.startCycle()
         expect(trafficLight.getState()).toBe('green')
     })
 
     it('knows that after 30 seconds have passed and the traffic light is on and currently green to switch to yellow', () => {
-        trafficLight.startCycle()
         jest.advanceTimersByTime(30000)
         expect(trafficLight.getState()).toBe('yellow')
     })
 
-    it ('knows that after 35 seconds, have passed and the traffic light was turned on, to switch to red', () => {
-        trafficLight.startCycle()
+    it ('knows that after 35 seconds have passed and the traffic light was turned on, to switch to red', () => {
         jest.advanceTimersByTime(35000)
         expect(trafficLight.getState()).toBe('red')
     })
+
 
 })
