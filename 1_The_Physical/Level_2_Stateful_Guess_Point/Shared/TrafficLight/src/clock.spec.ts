3b54489e9debd13d91cfadd6c78b63b6956a88f9
Lsiopongco <luigisiops@gmail.com>
2023-06-01 03:11:50 -0400

    [Refactor]: moved jest faktimers call and clock creation to before each

--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TrafficLight/src/clock.spec.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TrafficLight/src/clock.spec.ts
@@ -1,22 +1,22 @@
 import {Clock} from './clock'
 
 describe('clock system', () => {
+    let clock: Clock;
+    beforeEach(() => {
+        jest.useFakeTimers()
+        clock = Clock.create()
+    })
     it('knows that given a clock, the start time should be 0', ()=> {
-        const clock = Clock.create()
         expect(clock.getTime()).toEqual(0)
     })
 
     it('knows that given a clock, and we start the timer, after 10 seconds the current time should be 10', () => {
-        jest.useFakeTimers()
-        const clock = Clock.create()
         clock.startTime()
         jest.advanceTimersByTime(10000)
         expect(clock.getTime()).toEqual(10)
     })
 
     it('knows that given a clock and we start the timer, if 3 seconds pass and we stop the time for 20 more seconds it should still be at 3', () => {
-        jest.useFakeTimers()
-        const clock = Clock.create()
         clock.startTime()
         jest.advanceTimersByTime(3000)
         clock.stopTime()
