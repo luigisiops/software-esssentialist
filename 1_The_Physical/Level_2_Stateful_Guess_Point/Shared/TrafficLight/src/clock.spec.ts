598d68365818802080f9653c831444439d486be8
Lsiopongco <luigisiops@gmail.com>
2023-06-01 03:10:26 -0400

    [New]: created test to check that time remains the same after stopping the clock

--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TrafficLight/src/clock.spec.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TrafficLight/src/clock.spec.ts
@@ -13,4 +13,14 @@ describe('clock system', () => {
         jest.advanceTimersByTime(10000)
         expect(clock.getTime()).toEqual(10)
     })
+
+    it('knows that given a clock and we start the timer, if 3 seconds pass and we stop the time for 20 more seconds it should still be at 3', () => {
+        jest.useFakeTimers()
+        const clock = Clock.create()
+        clock.startTime()
+        jest.advanceTimersByTime(3000)
+        clock.stopTime()
+        jest.advanceTimersByTime(20000)
+        expect(clock.getTime()).toEqual(3)
+    })
 })
