de445e2fe8fafdc94ee372fc7a9c8c64b2757221
Lsiopongco <luigisiops@gmail.com>
2023-06-01 03:06:28 -0400

    [New]: added test to check clock time to equal 10 after starting the clock and 10 seconds pass

--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TrafficLight/src/clock.spec.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TrafficLight/src/clock.spec.ts
@@ -5,4 +5,12 @@ describe('clock system', () => {
         const clock = Clock.create()
         expect(clock.getTime()).toEqual(0)
     })
+
+    it('knows that given a clock, and we start the timer, after 10 seconds the current time should be 10', () => {
+        jest.useFakeTimers()
+        const clock = Clock.create()
+        clock.startTime()
+        jest.advanceTimersByTime(10000)
+        expect(clock.getTime()).toEqual(10)
+    })
 })
